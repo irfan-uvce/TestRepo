bdeadf039498e49efdc59621c357208cbd5e75b0
Irfan-uvce <irfan.uvce@gmail.com>
2022-01-20 08:43:26 -0800

    TEST3 changed from branch

--- src/main/java/Test3.java
+++ src/main/java/Test3.java
@@ -1,5 +1,7 @@
 public class Test3 {
     public static void main(String args[]) {
+
         System.out.println("This is Master 3");
+        System.out.println("This is Master but changed from the branch");
     }
 }
